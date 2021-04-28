9043b29955a8116d21e9934b26030e7aad15a238
jos <joseluisalba@joumma.com>
2021-04-28 11:38:59 +0200

    update

--- client/migrar/import.php
+++ client/migrar/import.php
@@ -16,7 +16,7 @@ class import
     /**
      * Endpoint
      */
-    public $endpoint;
+    public $endpoint = '';
 
      /**
      * Recupera todos los productos del sistema.
@@ -91,13 +91,17 @@ class import
         $url = $this->endpoint.'?pid='.$pid.'&cid='.$cid;
 
         //Curl
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-        $result = curl_exec($ch);
-        curl_close($ch);
+        $ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$BUFFERO = curl_exec($ch);
+		curl_close($ch);
 
-        return json_decode($result,true);
+        return json_decode($BUFFERO,true);
     }
 }
 
 $e = new import();
+//Importar productos.
+$e->getAllProduct();
